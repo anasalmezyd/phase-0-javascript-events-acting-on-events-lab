7036e7857754ed83f746526e3c6f83ea8f1e6032
anasalmezyd <Anas.almziad@gmail.com>
2023-07-08 09:50:48 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Your code here
+function moveDodgerLeft() {
+    // Extract the current left position
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    // Check that dodger is not going past the left edge of the game field
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+  function moveDodgerRight() {
+    // Extract the current left position
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+    
+    // Check that dodger is within game field boundaries (360 is calculated as 400 - 40, where 400 is the game field width and 40 is the dodger width)
+    if (left < 360) {
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
+  
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+    else if (e.key === "ArrowRight") {
+      moveDodgerRight();
+    }
+  });
+  
+    
